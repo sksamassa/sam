cb17c3b533fa072736e3d16d01c5e4fcb8c6c550
Sekou Samassa <sksamassa@gmail.com>
2024-11-09 22:31:36 +0500

    set up the project

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+import React from 'react'
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>Welcome to Sam! A real-time video webapp!</div>;
+}
