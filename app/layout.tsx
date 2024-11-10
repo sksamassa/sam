f1875e5d72f88988429b931a25c089a7b00384fd
Sekou Samassa <sksamassa@gmail.com>
2024-11-10 18:04:38 +0500

    implement authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
-import React from 'react'
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function RootLayout({
   children,
@@ -7,12 +8,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-      className={`bg-dark-2 text-white`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          logoImageUrl: "/icons/sam-logo.svg",
+          socialButtonsVariant: "iconButton",
+        },
+        variables: {
+          colorText: "#fff",
+          colorPrimary: "#0E78F9",
+          colorBackground: "#1c1f2e",
+          colorInputBackground: "#252a41",
+          colorInputText: "#fff",
+        },
+      }}
+    >
+      <html lang="en">
+        <body className={`bg-dark-2 text-white`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
